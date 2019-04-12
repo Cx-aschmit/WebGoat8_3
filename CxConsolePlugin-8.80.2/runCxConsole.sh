7627ff80ea66e10e12ab91da4a17f69103653a0d
Cx-aschmit <46612825+Cx-aschmit@users.noreply.github.com>
2019-04-12 15:26:39 -0400

    Update runCxConsole.sh

--- CxConsolePlugin-8.80.2/runCxConsole.sh
+++ CxConsolePlugin-8.80.2/runCxConsole.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "$(dirname "$0")"
-#java -Xmx1024m -jar CxConsolePlugin-CLI-8.80.2.jar "$@"
 echo "$@"
 echo "ls"
 ls
 
 echo "pwd"
 pwd
+
+cd "$(dirname "$0")"
+java -Xmx1024m -jar CxConsolePlugin-CLI-8.80.2.jar "$@"
